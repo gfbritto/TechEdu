4621edfa6d5ed2a1afea580aeca26748d4831a27
Gabriel Brito <gabriel.brito@take.net>
2022-05-04 09:33:52 -0300

    Add project files.

---
+++ TechEdu/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TechEdu.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
