b7914d70b7be0e0372373b1c27e0796b60ef4155
Gabriel Brito <gabriel.brito@take.net>
2022-05-08 16:58:24 -0300

    feat: add student controller

---
+++ TechEdu/Controllers/StudentsController.cs
@@ -0,0 +1,162 @@
+﻿#nullable disable
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TechEdu.Models.DataAccess.DataObjects;
+
+namespace TechEdu.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly colegioContext _context;
+
+        public StudentsController(colegioContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Students
+        public async Task<IActionResult> Index()
+        {
+            var colegioContext = _context.Alunos.Include(a => a.Responsavel).Include(a => a.Turma);
+            return View(await colegioContext.ToListAsync());
+        }
+
+        // GET: Students/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var aluno = await _context.Alunos
+                .Include(a => a.Responsavel)
+                .Include(a => a.Turma)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+
+            return View(aluno);
+        }
+
+        // GET: Students/Create
+        public IActionResult Create()
+        {
+            ViewData["ResponsavelId"] = new SelectList(_context.Responsavels, "Id", "Id");
+            ViewData["TurmaId"] = new SelectList(_context.Turmas, "Id", "Id");
+            return View();
+        }
+
+        // POST: Students/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,TurmaId,Ra,ResponsavelId,PrimeiroNome,UltimoNome,DataNascimento")] Aluno aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(aluno);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ResponsavelId"] = new SelectList(_context.Responsavels, "Id", "Id", aluno.ResponsavelId);
+            ViewData["TurmaId"] = new SelectList(_context.Turmas, "Id", "Id", aluno.TurmaId);
+            return View(aluno);
+        }
+
+        // GET: Students/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var aluno = await _context.Alunos.FindAsync(id);
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+            ViewData["ResponsavelId"] = new SelectList(_context.Responsavels, "Id", "Id", aluno.ResponsavelId);
+            ViewData["TurmaId"] = new SelectList(_context.Turmas, "Id", "Id", aluno.TurmaId);
+            return View(aluno);
+        }
+
+        // POST: Students/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,TurmaId,Ra,ResponsavelId,PrimeiroNome,UltimoNome,DataNascimento")] Aluno aluno)
+        {
+            if (id != aluno.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(aluno);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AlunoExists(aluno.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ResponsavelId"] = new SelectList(_context.Responsavels, "Id", "Id", aluno.ResponsavelId);
+            ViewData["TurmaId"] = new SelectList(_context.Turmas, "Id", "Id", aluno.TurmaId);
+            return View(aluno);
+        }
+
+        // GET: Students/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var aluno = await _context.Alunos
+                .Include(a => a.Responsavel)
+                .Include(a => a.Turma)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+
+            return View(aluno);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var aluno = await _context.Alunos.FindAsync(id);
+            _context.Alunos.Remove(aluno);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AlunoExists(int id)
+        {
+            return _context.Alunos.Any(e => e.Id == id);
+        }
+    }
+}
